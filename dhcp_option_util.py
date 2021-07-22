b6e4ff29ac9db80c54ed62026f7ba2f8ef020a2d
Chris Marrison <chris@infoblox.com>
2021-07-22 18:52:28 +0100

    20210721 v0.0.1 Working prototype

---
+++ dhcp_option_util.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+"""
+------------------------------------------------------------------------
+
+ Description:
+  DHCP Hex Encoding/Decoding Utility for Option Encoding
+
+ Requirements:
+  bloxone
+  yaml
+
+ Usage:
+
+ Author: Chris Marrison
+
+ Date Last Updated: 20210720
+
+Copyright 2021 Chris Marrison / Infoblox
+
+Redistribution and use in source and binary forms,
+with or without modification, are permitted provided
+that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+------------------------------------------------------------------------
+"""
+__version__ = '0.0.1'
+__author__ = 'Chris Marrison'
+__email__ = 'chris@infoblox.com'
+
+import bloxone
+import argparse
+import logging
+import os
+
+
+def parseargs():
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='DHCP Option Encoding and Decoding Utility')
+    parser.add_argument('-c', '--config', action="store", help="Path to database file", required=True)
+    parser.add_argument('-p', '--output_path', action="store", default='', help="Output file path (optional)")
+    parser.add_argument('--dump', type=str, default='', help="Dump Vendor")
+    parser.add_argument('--vendor', nargs=2, type=str, default='', help="Vendor Identifier")
+    parser.add_argument('-v', '--version', action='store_true', help="Config Version")
+    parser.add_argument('-y', '--yaml', action="store", help="Alternate yaml config file for objects")
+    parser.add_argument('--debug', help="Enable debug logging", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
+
+    return parser.parse_args()
+
+
+def main():
+    '''
+    Main logic
+    '''
+    exitcode = 0
+    options = parseargs()
+
+    definitions = bloxone.DHCP_OPTION_DEFS(options.config)
+    dhcp_encoder = bloxone.dhcp_encode()
+
+    for vendor in definitions.vendors():
+        sub_opts = definitions.sub_options(vendor)
+        if len(sub_opts):
+            encoded_opts = dhcp_encoder.encode_dhcp_option(sub_opts)
+            print(f'Vendor: {vendor}, Encoding: {encoded_opts}')
+        else:
+            print(f'Vendor: {vendor} has no sub-options to encode')
+        
+    return exitcode
+
+### Main ###
+if __name__ == '__main__':
+    exitcode = main()
+    exit(exitcode)
